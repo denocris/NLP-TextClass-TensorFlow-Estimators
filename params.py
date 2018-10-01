d6104ddf897450bff667b6670ad3d775824ccd61
asr <denocris>
2018-10-01 13:50:29 +0200

    params for six problem

--- params.py
+++ params.py
@@ -8,17 +8,17 @@ import tensorflow as tf
 # ------------ ANN MODEL -------------
 # ------------------------------------
 
-#MODEL_FN = models.lstm_model_fn
-MODEL_FN = models.cnn_model_fn
+MODEL_FN = models.lstm_model_fn
+#MODEL_FN = models.cnn_model_fn
 
-PRINT_SHAPE = True
+PRINT_SHAPE = False
 # ------------------------------------
 # ----- SETUP TRAINING--------
 # ------------------------------------
 RESUME_TRAINING = False
 MULTI_THREADING = True
-TRAIN_SIZE = 317890 #159598 #1429485
-NUM_EPOCHS = 1
+TRAIN_SIZE = 54045 #317890 #159598 #1429485
+NUM_EPOCHS = 5
 BATCH_SIZE = 128
 EVAL_AFTER_SEC = 120
 TOTAL_STEPS = int((TRAIN_SIZE/BATCH_SIZE)*NUM_EPOCHS)
@@ -45,9 +45,9 @@ LEARNING_RATE = 0.01
 # For LSTM0
 FORGET_BIAS=1.0
 # For LSTM0
-DROPOUT_RATE = 0.12
+DROPOUT_RATE = 0.2
 # For LSTM it refers to the size of the Cell, for CNN model instead are the FC layers
-HIDDEN_UNITS = [8,4] #[96, 64, 16], None
+HIDDEN_UNITS = [64, 32, 16] #[96, 64, 16], None
 # For CNN, kernel size
 WINDOW_SIZE = 3
 # For CNN, number of filters (i.e. feature maps)
@@ -63,9 +63,13 @@ INFERENCE_DIR = os.path.join(os.getcwd(), 'inference_results/')
 # ------------------------------------
 # ------- TRAIN & VALID PATH ---------
 # ------------------------------------
-TRAIN_DATA_FILES_PATTERN = os.path.join(os.getcwd(),'data/train-data-maxlength16-subtitles.tsv')
-VALID_DATA_FILES_PATTERN = os.path.join(os.getcwd(),'data/valid-data-maxlength16-subtitles.tsv')
-VOCAB_LIST_FILE = os.path.join(os.getcwd(),'vocab/vocab_list_5k_2k_mystop_nodgts.tsv')
-N_WORDS_FILE = os.path.join(os.getcwd(),'vocab/n_words_5k_2k_mystop_nodgts.tsv')
+# TRAIN_DATA_FILES_PATTERN = os.path.join(os.getcwd(),'data/train-data-maxlength16-subtitles.tsv')
+# VALID_DATA_FILES_PATTERN = os.path.join(os.getcwd(),'data/valid-data-maxlength16-subtitles.tsv')
+# VOCAB_LIST_FILE = os.path.join(os.getcwd(),'vocab/vocab_list_5k_2k_mystop_nodgts.tsv')
+# N_WORDS_FILE = os.path.join(os.getcwd(),'vocab/n_words_5k_2k_mystop_nodgts.tsv')
+TRAIN_DATA_FILES_PATTERN = os.path.join(os.getcwd(),'data_six/train_data_length3-16.tsv')
+VALID_DATA_FILES_PATTERN = os.path.join(os.getcwd(),'data_six/valid_data_length3-16.tsv')
+VOCAB_LIST_FILE = os.path.join(os.getcwd(),'data_six/vocab_list_5k.tsv')
+N_WORDS_FILE = os.path.join(os.getcwd(),'data_six/n_words_5k.tsv')
 with open(N_WORDS_FILE) as file:
     N_WORDS = int(file.read())+2
