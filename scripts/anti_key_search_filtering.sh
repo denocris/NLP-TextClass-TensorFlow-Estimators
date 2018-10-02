4f086469f3539588875ed3b793b1a7ee51586c46
asr <denocris>
2018-10-02 17:30:41 +0200

    added anti_key script and descriptions

---
+++ scripts/anti_key_search_filtering.sh
@@ -0,0 +1,47 @@
+anti_key_output#!/bin/sh
+
+# DESCRIPTION
+# This script performs an 'anti key search' filtering. i.e. it filters
+# from a corpus only rows that do NOT contain selected keys/words
+# which are listed in a .txt file
+
+# Usage:$: time bash anti_key_search_v1.sh <text_to_filter> <keys.txt>
+
+# remove old key_words_output
+rm -rf anti_key_count
+
+text=$1
+keysfile=$2
+
+# list all the key words
+#"(mese|settimana)\s.*(prossim[oa]|scors[oa])"
+declare -a keys
+readarray keys < $keysfile
+
+
+init_wc=$(cat $text | wc -l)
+echo "------ init_wc ---------" $init_wc
+tot=0
+
+## now loop through the key words
+for key in "${keys[@]}"
+do
+   echo $key
+   #echo $(cat $text | grep -w "$")
+   rm -rf anti_key_output
+   cat $text | grep -vwP $key >> anti_key_output
+   rm -rf anti_key_output_tmp
+   out_wc=$(cat anti_key_output | wc -l)
+   num_sentences=$(($init_wc-$out_wc))
+   echo "-------- num --------" $num_sentences
+   echo -e $key '\t' $num_sentences >> anti_key_count
+   init_wc=$out_wc
+   echo "-------- init_wc --------" $init_wc
+   tot=$(($tot + $num_sentences))
+   echo "-------- tot --------" $tot
+   cp anti_key_output anti_key_output_tmp
+   text=anti_key_output_tmp
+done
+
+
+echo -e 'total' '\t' $tot >> anti_key_count
